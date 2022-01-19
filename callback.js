b7e8859e96bed9b32dee0d3e308be0f147484d3e
Lucas Wesley <contlucas@outlook.com>
2022-01-19 18:13:08 -0300

    Meu primeiro

---
+++ callback.js
@@ -0,0 +1,42 @@
+// // function dobro (a){
+// //     return a*2
+// //     };
+// //     function triplo (a){
+// //         return a*3 
+// //     };
+// //     function aplicar(a,callback){
+// //         return  callback(a)
+// //     }
+    
+// //     console.log(dobro(5));
+// //     console.log(triplo(5));
+// //     console.log(aplicar(10,dobro));
+// //     console.log(aplicar(10,triplo));
+
+// function somar (a,b){
+//     return a+b
+// };
+// function subtrair (a,b){
+//     return a-b
+// };
+// function multiplicar (a,b){
+//     return a*b
+//     };
+// function dividir (a,b){
+//     return a/b
+// };
+// function calculadora (a,b,callback){
+//     return callback(a,b)
+// };
+
+// console.log(calculadora(2,2,somar))
+
+function adicionarHttp (a) {
+    for (var i=0; i<a.length;i++){   return "http://"+ a[i]
+    }
+    };
+    function processar(lista,callback){
+        return callback(lista)
+
+    };
+
